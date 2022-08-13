36890023113c14bac485cc36ecf2f17655308e30
Adrian Batista <adrianfnb@gmail.com>
2022-08-12 21:19:35 -0300

    React index page

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="nav">
+            <img src="./logo192.png" className="nav-logo" />
+            <h3 className="nav-h3">ReactFacts</h3>
+            <h4 className="nav-h4">React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default Navbar
